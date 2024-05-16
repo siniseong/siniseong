c994eab659eba809243b74bb1c7955a3373a1103
huise0ng <shinhuiseong07@gmail.com>
2024-05-16 15:27:57 +0900

    FIX :: 로고 변경

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
-      favicon: path.resolve(__dirname, 'src', 'favicon.ico'),
+      favicon: path.resolve(__dirname, 'src', 'logo.png'),
     }),
   ],
   target: ['web', 'es5'],
